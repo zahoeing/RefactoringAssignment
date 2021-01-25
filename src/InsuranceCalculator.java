47489f19ba071e3cc6c774fda50b4e775b434134
zhoeing <zahoeing@bsu.edu>
2021-01-24 21:05:15 -0500

    Extracting methods

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -8,8 +8,24 @@ public class InsuranceCalculator {
         } else if (income <= 60000) {
             return (income-30000)*0.1+76500;
         } else {
-            return (income-60000)*0.02+105600;
+            return calculateInsuranceVeryHigh(income);
         }
+    }
+
+    public double calculateInsuranceVeryHigh(double income) {
+        return (income- getAdjustment())* getWeight() + getConstant();
+    }
+
+    public int getConstant() {
+        return 105600;
+    }
+
+    public double getWeight() {
+        return 0.02;
+    }
+
+    public int getAdjustment() {
+        return 60000;
     }
 
 }
